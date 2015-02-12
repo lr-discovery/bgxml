501e3f54c1de7d4c7ae1b2c172d83d833db41028
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2015-02-12 11:00:05 +0000

    Better Unit Tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,36 @@
 import unittest
 import urllib2
+from app.SampleApp import app
+
 
 class TestWebService(unittest.TestCase):
 
-#    def setUp(self):
+    def setUp(self):
+        self.app = app.test_client()
 #        print os.basedir
 #        print os.path
+
+    def test_service_name_incorrect(self):
+        with open('BgTest.xml') as source_data:
+            soap_body = source_data.read()
+        headers = {'Content-Type': 'text/xml'}
+        response = self.app.post('/soap/nosuchservice', data = soap_body, headers = headers)
+        self.assertEqual(response.status, '404 NOT FOUND')
+
+    def test_service_with_good_xml_data(self):
+        with open('BgTest.xml') as source_data:
+            soap_body = source_data.read()
+        headers = {'Content-Type': 'text/xml'}
+        response = self.app.post('/soap/bgservice/reflection', data = soap_body, headers = headers)
+        self.assertEqual(response.status, '200 OK')
+
+    def test_service_with_bad_xml_data(self):
+        with open('BgBad.xml') as source_data:
+            soap_body = source_data.read()
+        headers = {'Content-Type': 'text/xml'}
+        response = self.app.post('/soap/bgservice/reflection', data = soap_body, headers = headers)
+        self.assertEqual(response.status, '500 Internal Server Error')
+
 
     def test_reflection(self):
         with open('BgTest.xml') as source_data:
