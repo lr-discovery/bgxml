1478e9b6239f7602a4698fa5092cf2748f6f41b1
SimonRAllis <simon.allis@digital.landregistry.gov.uk>
2015-02-12 16:20:09 +0000

    add new function

--- app/SampleApp.py
+++ app/SampleApp.py
@@ -3,22 +3,33 @@ from flask.ext.spyne import Spyne
 from spyne.protocol.soap import Soap11
 from spyne.model.primitive import Unicode, Integer
 from spyne.model.complex import Iterable
+from random import randint
+
 
 app = Flask(__name__)
 spyne = Spyne(app)
 
+
 class BgSoapService(spyne.Service):
-    __service_url_path__ = '/soap/bgservice/reflection'
+    __service_url_path__ = '/soap/bgservice'
     __in_protocol__ = Soap11(validator='lxml')
     __out_protocol__ = Soap11()
 
+
     @spyne.srpc(Unicode, Unicode, Unicode, Integer, _returns=Iterable(Unicode))
-    def bg(title, address, application, id):
+    def reflection(title, address, application, id):
 
         yield title
         yield address
         yield application
         yield str(id)
+
+    @spyne.srpc(Unicode, Unicode, Unicode, _returns=Unicode)
+    def tran(title, address, application):
+
+        static_id = randint(0,100)
+        print static_id
+        return str(static_id)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
