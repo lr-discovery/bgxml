7d9cd8c450ed6081d2908dfefa70f89dc8bf2d2e
SimonRAllis <simon.allis@digital.landregistry.gov.uk>
2015-02-12 11:08:44 +0000

    TestHarness

--- runTest.sh
+++ runTest.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 echo 'Bg test script'
 echo '--------------'
+echo
+echo 'Service wsdl saved to TestOutputWsdl.xml'
+curl http://localhost:5000/soap/bgservice/reflection?wsdl > TestOutputWsdl.xml
+echo
+echo
 echo 'Inputed good xml'
 cat BgTest.xml
-echo 'Returned result'
-curl --data @BgTest.xml http://localhost:5000/soap/bgservice/reflection
+echo
+echo 'Returned xml saved to TestOutputGood.xml'
+curl --data @BgTest.xml http://localhost:5000/soap/bgservice/reflection > TestOutputGood.xml
+echo
+echo
 echo 'Inputed bad xml'
 cat BgBad.xml
 echo
-echo 'Returned result'
-curl --data @BgBad.xml http://localhost:5000/soap/bgservice/reflection
+echo
+echo 'Returned xml saved to TestOutputBad.xml'
+curl --data @BgBad.xml http://localhost:5000/soap/bgservice/reflection > TestOutputBad.xml
 exit
